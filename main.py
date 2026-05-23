aa84fd02cd16753c31889ce8f04cc8652f90cfa2
oliverrudow <oliver.rudow@googlemail.com>
2026-05-23 19:41:50 +0200

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+from yfinance_me import myYFinance
+
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print_hi('PyCharm')
+    my_y_fiance = myYFinance.MyYFinance()
+    str_isin = 'US0378331005'
+    my_y_fiance.set_actual_quote_invest_status(True)
+    my_y_fiance.set_actual_quote_isin(str_isin)
+    print(my_y_fiance.get_actual_quote_dict_static_watch_list_data)
+    print('-------------------------------------------------')
+    print(my_y_fiance.get_actual_quote_dict_performance_watch_list_data)
+    print('-------------------------------------------------')
+    print(my_y_fiance.get_actual_quote_dict_analyst_watch_list_data)
+    print('-------------------------------------------------')
+    print(my_y_fiance.get_actual_quote_dict_fundamentals_watch_list_data)
+    print('-------------------------------------------------')
+    print(my_y_fiance.get_actual_quote_dict_derivate_watch_list_data)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='myYFinance',
+    version='0.1.1',
+    packages=['yfinance_me'],
+    url='',
+    license='',
+    author='Oliver Rudow',
+    author_email='oliver.rudow@googlemail.com',
+    description='my yfinance',
+    install_requires=['pandas', 'yfinance'],
+)
